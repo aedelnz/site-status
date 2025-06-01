b0e43fc4946fe53711b07ff106c2fd91dc335e0d
aedelnz <aedelnz@qq.com>
2025-06-01 17:29:16 +0800

    内容优化

--- public/config.js
+++ public/config.js
@@ -1,17 +1,13 @@
 window.Config = {
-
   // 显示标题
   SiteName: '画的站点',
-
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'ur1466828-96640b6a19c5204706e00c05',
   ],
-
   // 日志天数
   CountDays: 30,
-
   // 导航栏菜单
   Navi: [
     {
@@ -20,7 +16,6 @@ window.Config = {
     },{
       text: '原页',
       url: 'https://stats.uptimerobot.com/0M2zqUkp83'
-    },
-
+    }
   ],
 };
